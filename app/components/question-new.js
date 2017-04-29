396784143d0e5036e9012867d2fb4f7b85c58186
Chris Carr <coder.knot@gmail.com>
2017-04-29 12:25:20 -0700

    add functionality to save new question to question-new.js

--- app/components/question-new.js
+++ app/components/question-new.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions:{
+    saveQuestion() {
+      var params = {
+        author: this.get('author'),
+        title: this.get('title'),
+        content: this.get('content')
+      }
+      this.sendAction('saveQuestion', params);
+    }
+  }
 });
