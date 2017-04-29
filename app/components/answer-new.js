3bdd5761bb5d60cbf8f2604e5beed2a0a33c1543
Chris Carr <coder.knot@gmail.com>
2017-04-29 16:38:49 -0700

    add functionality to save new answer to answer-new.js

--- app/components/answer-new.js
+++ app/components/answer-new.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  newAnswerForm: false,
+  actions: {
+    newAnswerForm() {
+      this.set('newAnswerForm', true);
+    },
+    saveAnswer() {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+        question: this.get('question')
+      };
+      this.set('newAnswerForm', false);
+      this.sendAction('saveAnswer', params);
+    }
+  }
 });
